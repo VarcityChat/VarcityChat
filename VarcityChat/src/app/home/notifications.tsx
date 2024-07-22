ac15a02a08fd7c407e271b6bdba1177b6eaefbb6
Sheggsmann <promisesheggs@gmail.com>
2024-07-22 16:45:43 -0700

    chore: revised notifications screen

--- VarcityChat/src/app/home/notifications.tsx
+++ VarcityChat/src/app/home/notifications.tsx
@@ -1,27 +1,25 @@
-import BackButton from "@/components/back-button";
+import Header from "@/components/header";
 import { View, Text, TouchableOpacity, List } from "@/ui";
 import { useRouter } from "expo-router";
-import { SafeAreaView } from "react-native";
 
 export default function NotificationsScreen() {
   const router = useRouter();
   return (
-    <SafeAreaView className="flex flex-1">
-      <View className="flex flex-row items-center px-6">
-        <BackButton onPress={() => router.back()} />
-        <View className="flex flex-1 items-center">
-          <Text className="font-bold text-lg">Notification</Text>
-        </View>
-        <TouchableOpacity>
-          <Text className="text-primary-600">mark all as read</Text>
-        </TouchableOpacity>
-      </View>
-
-      <List
-        data={[1, 2, 3, 4, 5, 6]}
-        renderItem={({}) => <View></View>}
-        estimatedItemSize={50}
-      />
-    </SafeAreaView>
+    <View className="flex flex-1">
+      <Header
+        title="Notification"
+        headerRight={
+          <TouchableOpacity activeOpacity={0.7}>
+            <Text className="text-primary-500">mark all as read</Text>
+          </TouchableOpacity>
+        }
+      >
+        <List
+          data={[1, 2, 3, 4, 5, 6]}
+          renderItem={({}) => <View></View>}
+          estimatedItemSize={50}
+        />
+      </Header>
+    </View>
   );
 }
