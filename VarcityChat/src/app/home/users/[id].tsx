0cd6cec890e58b169a56baefc88f273195e1336a
Sheggsmann <promisesheggs@gmail.com>
2024-10-27 00:33:30 -0700

    fix: user profile detail page animation now working

--- VarcityChat/src/app/home/users/[id].tsx
+++ VarcityChat/src/app/home/users/[id].tsx
@@ -1,17 +1,22 @@
 import BackButton from "@/components/back-button";
-import { View, Text, WIDTH, colors } from "@/ui";
+import { View, Text, WIDTH } from "@/ui";
 import { userImg } from "@/ui/images";
-import { Stack, useRouter } from "expo-router";
+import { useRouter } from "expo-router";
 import { TouchableOpacity, FlatList } from "react-native";
-import ChatsSvg from "@/ui/icons/chats";
 import Animated, {
   interpolate,
   useAnimatedRef,
+  useAnimatedScrollHandler,
   useAnimatedStyle,
   useScrollViewOffset,
+  useSharedValue,
+  withTiming,
 } from "react-native-reanimated";
 import { useSafeAreaInsets } from "react-native-safe-area-context";
 import { useColorScheme } from "nativewind";
+import CallsActive from "@/ui/icons/calls-active";
+import ChatUserSvg from "@/ui/icons/user/chat-user-svg";
+import { HEADER_HEIGHT } from "@/components/header";
 
 const IMG_HEIGHT = 300;
 
@@ -45,149 +50,171 @@ export default function User() {
     };
   });
 
-  const headerAnimatedStyle = useAnimatedStyle(() => {
-    return {
-      opacity: interpolate(scrollOffset.value, [0, IMG_HEIGHT / 2], [0, 1]),
-    };
+  const scrollY = useSharedValue(0);
+
+  const scrollHandler = useAnimatedScrollHandler({
+    onScroll: (event) => {
+      scrollY.value = event.contentOffset.y;
+    },
   });
 
   const headerTextAnimatedStyle = useAnimatedStyle(() => {
     return {
       opacity: interpolate(
-        scrollOffset.value,
-        [0, IMG_HEIGHT / 2, IMG_HEIGHT + 40],
+        scrollY.value,
+        [0, IMG_HEIGHT / 2, IMG_HEIGHT],
         [0, 0, 1]
       ),
     };
   });
 
+  const headerStyle = useAnimatedStyle(() => {
+    return {
+      opacity: interpolate(scrollY.value, [0, IMG_HEIGHT / 2], [0, 1]),
+      borderBottomWidth: withTiming(scrollY.value > 0 ? 0.5 : 0, {
+        duration: 300,
+      }),
+      elevation: withTiming(scrollY.value > 0 ? 4 : 0, { duration: 300 }),
+    };
+  });
+
   return (
     <>
-      <Stack.Screen
-        options={{
-          headerTransparent: true,
-          headerTitle: () => (
-            <Animated.Text
-              className={"font-semibold text-lg dark:text-white"}
-              style={[headerTextAnimatedStyle]}
-            >
-              Ebuka Varcity
-            </Animated.Text>
-          ),
-          headerBackground: () => (
-            <Animated.View
-              style={[
-                {
-                  height: 100,
-                  backgroundColor: colorScheme === "dark" ? "#151718" : "#fff",
-                },
-                headerAnimatedStyle,
-              ]}
-            ></Animated.View>
-          ),
-        }}
-      />
-
-      <Animated.ScrollView ref={scrollRef} scrollEventThrottle={16}>
-        <View className="relative">
-          <FlatList
-            data={[userImg, userImg, userImg]}
-            horizontal
-            keyExtractor={(item, index) => `img-${index}`}
-            renderItem={({ item }) => (
-              <Animated.Image
-                source={item}
-                style={[
-                  { width: WIDTH, height: IMG_HEIGHT, objectFit: "cover" },
-                ]}
-              />
-            )}
-            pagingEnabled
-            bounces={false}
-          />
-
-          {/* <Animated.Image
-            source={userImg}
-            style={[
-              {
-                width: WIDTH,
-                height: IMG_HEIGHT,
-                objectFit: "cover",
-              },
-              imageAnimatedStyle,
-            ]}
-          /> */}
-
-          <View
-            className="absolute left-6 z-50"
-            style={{ top: insets.top + 10 }}
-          >
-            <BackButton
-              onPress={() => {
-                router.back();
-              }}
-            />
-          </View>
-        </View>
-
-        <View className="min-h-screen px-6 py-8">
-          <View className="flex-row">
+      {/* HEADER */}
+      <View className="flex flex-1">
+        <Animated.View
+          className="absolute top-0 left-0 right-0 bg-white z-10 border-b-grey-100 dark:bg-charcoal-950 dark:border-b-charcoal-800 opacity-0"
+          style={[
+            headerStyle,
+            { paddingTop: insets.top, height: HEADER_HEIGHT + insets.top },
+          ]}
+        >
+          <View className="flex flex-row items-center justify-between py-4 px-6">
             <View className="flex-1">
-              <Animated.Text className="font-bold text-2xl text-black dark:text-white">
+              <BackButton
+                onPress={() => {
+                  router.canGoBack() && router.back();
+                }}
+              />
+            </View>
+            <View className="items-center">
+              <Animated.Text
+                className="font-semibold text-lg dark:text-white"
+                style={[headerTextAnimatedStyle]}
+              >
                 Ebuka Varcity
               </Animated.Text>
-              <Text className="text-grey-500 dark:text-grey-200">
-                Lead City University
-              </Text>
-              <Text className="text-grey-500 dark:text-grey-200">
-                Computer Science
-              </Text>
-              <Text className="text-grey-500 dark:text-grey-200">
-                100 Level
-              </Text>
-            </View>
-
-            <View>
-              <TouchableOpacity
-                activeOpacity={0.7}
-                className="w-[40px] h-[40px] rounded-lg bg-primary-600 items-center justify-center"
-              >
-                <ChatsSvg fill={"white"} />
-              </TouchableOpacity>
             </View>
           </View>
+        </Animated.View>
 
-          <Seperator />
+        <Animated.ScrollView onScroll={scrollHandler} scrollEventThrottle={16}>
+          {/* Content Goes Here */}
 
-          <Text className="font-bold text-lg">About Me</Text>
-          <Text className="text-grey-500 dark:text-grey-200">
-            Lorem ipsum dolor sit amet consectetur. Integer arcu amet tempor
-            porttitor sapien varius. Dolor porttitor ut cursus ut vel facilisis
-            arcu tortor quam. Pellentesque urna non egestas nam elementum
-            feugiat risus urna bibendum. Integer accumsan non porttitor elit.
-            Aliquam velit nullam consectetur sed et. Mauris nunc massa dui ac
-            pretium. Dictum vel leo turpis massa facilisis fusce. Ultricies sit
-            massa eget id. Maecenas nulla aliquam pulvinar auctor. Faucibus
-            bibendum in enim feugiat. Et ac feugiat lectus.
-          </Text>
+          <Animated.ScrollView ref={scrollRef} scrollEventThrottle={16}>
+            <View className="relative flex flex-row bg-red-400">
+              <FlatList
+                data={[userImg, userImg, userImg]}
+                horizontal
+                keyExtractor={(item, index) => `img-${index}`}
+                renderItem={({ item }) => (
+                  <Animated.Image
+                    source={item}
+                    style={[
+                      { width: WIDTH, height: IMG_HEIGHT, objectFit: "cover" },
+                    ]}
+                  />
+                )}
+                pagingEnabled
+                bounces={false}
+              />
 
-          <Seperator />
-
-          <Text className="font-bold text-lg">Hobbies</Text>
-          <View className="flex-row gap-3 mt-4">
-            {hobbies.map((hobby, index) => (
               <View
-                className="p-3 rounded-full bg-primary-200 border border-primary-600"
-                key={`hobby-${index}`}
+                style={{
+                  position: "absolute",
+                  left: 16,
+                  top: insets.top + 10,
+                  zIndex: 99,
+                }}
               >
-                <Text className="text-primary-600 dark:text-primary-600">
-                  {hobby}
-                </Text>
+                <BackButton
+                  className="absolute z-50"
+                  onPress={() => {
+                    router.canGoBack() && router.back();
+                  }}
+                />
               </View>
-            ))}
-          </View>
-        </View>
-      </Animated.ScrollView>
+            </View>
+
+            <View className="min-h-screen px-6 py-8">
+              <View className="flex-row">
+                <View className="flex-1">
+                  <Animated.Text className="font-bold text-2xl text-black dark:text-white">
+                    Ebuka Varcity
+                  </Animated.Text>
+                  <Text className="text-grey-500 dark:text-grey-200">
+                    Lead City University
+                  </Text>
+                  <Text className="text-grey-500 dark:text-grey-200">
+                    Computer Science
+                  </Text>
+                  <Text className="text-grey-500 dark:text-grey-200">
+                    100 Level
+                  </Text>
+                </View>
+
+                <View className="flex flex-row gap-4">
+                  <TouchableOpacity
+                    activeOpacity={0.7}
+                    className="w-[45px] h-[45px] rounded-lg bg-primary-100 items-center justify-center border border-primary-500"
+                    onPress={() => alert("Call not available at the moment")}
+                  >
+                    <CallsActive />
+                  </TouchableOpacity>
+                  <TouchableOpacity
+                    activeOpacity={0.7}
+                    className="w-[45px] h-[45px] rounded-lg bg-primary-600 items-center justify-center border border-primary-600"
+                    onPress={() => router.push("/home/chat-message/1")}
+                  >
+                    <ChatUserSvg />
+                  </TouchableOpacity>
+                </View>
+              </View>
+
+              <Seperator />
+
+              <Text className="font-bold text-lg">About Me</Text>
+              <Text className="text-grey-500 dark:text-grey-200">
+                Lorem ipsum dolor sit amet consectetur. Integer arcu amet tempor
+                porttitor sapien varius. Dolor porttitor ut cursus ut vel
+                facilisis arcu tortor quam. Pellentesque urna non egestas nam
+                elementum feugiat risus urna bibendum. Integer accumsan non
+                porttitor elit. Aliquam velit nullam consectetur sed et. Mauris
+                nunc massa dui ac pretium. Dictum vel leo turpis massa facilisis
+                fusce. Ultricies sit massa eget id. Maecenas nulla aliquam
+                pulvinar auctor. Faucibus bibendum in enim feugiat. Et ac
+                feugiat lectus.
+              </Text>
+
+              <Seperator />
+
+              <Text className="font-bold text-lg">Hobbies</Text>
+              <View className="flex-row gap-3 mt-4">
+                {hobbies.map((hobby, index) => (
+                  <View
+                    className="p-3 rounded-full bg-primary-200 border border-primary-600"
+                    key={`hobby-${index}`}
+                  >
+                    <Text className="text-primary-600 dark:text-primary-600">
+                      {hobby}
+                    </Text>
+                  </View>
+                ))}
+              </View>
+            </View>
+          </Animated.ScrollView>
+        </Animated.ScrollView>
+      </View>
     </>
   );
 }
--- VarcityChat/src/components/back-button.tsx
+++ VarcityChat/src/components/back-button.tsx
@@ -1,12 +1,26 @@
-import { View, TouchableOpacity } from "@/ui";
+import { TouchableOpacity } from "@/ui";
 import { CaretBack } from "@/ui/icons/caret-back";
+import { useMemo } from "react";
+import { twMerge } from "tailwind-merge";
 
-export default function BackButton({ onPress }: { onPress: () => void }) {
+export default function BackButton({
+  onPress,
+  className,
+}: {
+  onPress: () => void;
+  className?: string;
+}) {
+  const buttonStyle = useMemo(
+    () =>
+      twMerge(
+        "flex w-[35px] h-[35px] justify-center items-center bg-grey-50 dark:bg-grey-800 rounded-md",
+        className
+      ),
+    [className]
+  );
+
   return (
-    <TouchableOpacity
-      className="flex w-[35px] h-[35px] justify-center items-center bg-grey-50 dark:bg-grey-800 rounded-md"
-      onPress={onPress}
-    >
+    <TouchableOpacity className={buttonStyle} onPress={onPress}>
       <CaretBack />
     </TouchableOpacity>
   );
