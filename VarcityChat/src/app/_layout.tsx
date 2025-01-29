0034618c022f28b0eb0ab8d839b935f0cef3673a
Sheggsmann <promisesheggs@gmail.com>
2025-01-29 23:28:40 +0100

    style: added a new font

--- VarcityChat/src/app/_layout.tsx
+++ VarcityChat/src/app/_layout.tsx
@@ -6,6 +6,23 @@ import { BottomSheetModalProvider } from "@gorhom/bottom-sheet";
 import { useThemeConfig } from "@/core/use-theme-config";
 import { ThemeProvider } from "@react-navigation/native";
 import { MenuProvider } from "react-native-popup-menu";
+import {
+  PlusJakartaSans_200ExtraLight,
+  PlusJakartaSans_200ExtraLight_Italic,
+  PlusJakartaSans_300Light,
+  PlusJakartaSans_300Light_Italic,
+  PlusJakartaSans_400Regular,
+  PlusJakartaSans_400Regular_Italic,
+  PlusJakartaSans_500Medium,
+  PlusJakartaSans_500Medium_Italic,
+  PlusJakartaSans_600SemiBold,
+  PlusJakartaSans_600SemiBold_Italic,
+  PlusJakartaSans_700Bold,
+  PlusJakartaSans_700Bold_Italic,
+  PlusJakartaSans_800ExtraBold,
+  PlusJakartaSans_800ExtraBold_Italic,
+  useFonts,
+} from "@expo-google-fonts/plus-jakarta-sans";
 
 export { ErrorBoundary } from "expo-router";
 
@@ -19,13 +36,32 @@ export default function RootLayout() {
 }
 
 function RootLayoutNav() {
+  const [fontsLoaded] = useFonts({
+    PlusJakartaSans_200ExtraLight,
+    PlusJakartaSans_300Light,
+    PlusJakartaSans_400Regular,
+    PlusJakartaSans_500Medium,
+    PlusJakartaSans_600SemiBold,
+    PlusJakartaSans_700Bold,
+    PlusJakartaSans_800ExtraBold,
+    PlusJakartaSans_200ExtraLight_Italic,
+    PlusJakartaSans_300Light_Italic,
+    PlusJakartaSans_400Regular_Italic,
+    PlusJakartaSans_500Medium_Italic,
+    PlusJakartaSans_600SemiBold_Italic,
+    PlusJakartaSans_700Bold_Italic,
+    PlusJakartaSans_800ExtraBold_Italic,
+  });
+
   const hideSplash = async () => {
     await SplashScreen.hideAsync();
   };
 
   useEffect(() => {
-    hideSplash();
-  }, []);
+    if (fontsLoaded) {
+      hideSplash();
+    }
+  }, [fontsLoaded]);
 
   return (
     <Providers>
