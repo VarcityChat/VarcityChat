d6e63848b03fc4ed353065dd87cb0c1c8a45e651
Sheggsmann <promisesheggs@gmail.com>
2025-02-28 18:39:54 +0100

    feat: fixed dark mode and light mode issue

--- VarcityChat/src/app/(app)/chat-message/_layout.tsx
+++ VarcityChat/src/app/(app)/chat-message/_layout.tsx
@@ -1,13 +1,16 @@
 import { Stack, useLocalSearchParams, useRouter } from "expo-router";
-import { Image, View, Text, TouchableOpacity, IS_IOS } from "@/ui";
+import { Image, View, Text, TouchableOpacity, IS_IOS, colors } from "@/ui";
 import ThreeDotsSvg from "@/ui/icons/three-dots";
 import CallsActive from "@/ui/icons/calls-active";
 import BackButton from "@/components/back-button";
 import { useActiveChat } from "@/core/hooks/use-chats";
 import { defaultAvatarUrl } from "../../../../constants/chats";
+import { useColorScheme } from "nativewind";
 
 export default function ChatMessageLayout() {
   const router = useRouter();
+  const { colorScheme } = useColorScheme();
+  const isDark = colorScheme === "dark";
   const { id } = useLocalSearchParams();
   const { activeChatReceiver } = useActiveChat(id as string);
 
@@ -16,6 +19,9 @@ export default function ChatMessageLayout() {
       <Stack.Screen
         name="[id]"
         options={{
+          headerStyle: {
+            backgroundColor: isDark ? colors.black : colors.white,
+          },
           title: "",
           headerBackTitleVisible: false,
           headerBackVisible: false,
